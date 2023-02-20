1c009cb558b80b25139fa719dc8e2c652f800600
Savenok <Romasav2017@gmail.com>
2023-02-20 15:22:03 +0300

    factory

---
+++ factory/LionsFactory.java
@@ -0,0 +1,18 @@
+﻿import java.util.ArrayList;
+
+import animals.*;
+import cage.*;
+import java.util.ArrayList;
+
+public class LionsFactory {
+
+    static public  ArrayList<Lion> createLions (int lionCount){
+        ArrayList<Lion> lionInCage = new ArrayList<>(lionCount);
+        for (int i = 0; i < lionCount; i++) {
+            Lion lion = new Lion(5.5, 8, 900, 4);
+            lionInCage.add(lion);
+        }
+        return lionInCage;
+    }
+
+}
