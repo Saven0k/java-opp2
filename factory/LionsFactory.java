bedc54880b0f44e8d4ef0f51b47e80a8b0f148f1
Savenok <Romasav2017@gmail.com>
2023-02-24 22:56:52 +0300

    factory

--- factory/LionsFactory.java
+++ factory/LionsFactory.java
@@ -1,13 +1,12 @@
 ﻿import java.util.ArrayList;
 import java.util.Random;
 
-import animals.*;
-import cage.*;
+
 import java.util.ArrayList;
 
 public class LionsFactory {
 
-     static public  ArrayList<Lion> createLions (int lionCount ){
+    static public ArrayList<Lion> createLions(int lionCount) {
         ArrayList<Lion> lionInCage = new ArrayList<>(lionCount);
         for (int i = 0; i < 5; i++) {
             Lion lion = new Lion(new Random().nextInt(10), 8, 900, 4);
