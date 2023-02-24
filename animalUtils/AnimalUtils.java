706ea5b92312f2891b02d451d4f79f75f7bc407f
Savenok <Romasav2017@gmail.com>
2023-02-24 22:56:16 +0300

    animalUtils

---
+++ animalUtils/AnimalUtils.java
@@ -0,0 +1,11 @@
+﻿import Comporator.AnimalComporator;
+
+import java.util.Collections;
+import java.util.List;
+
+public class AnimalUtils {
+    public static <T extends Animal> List<T> sortAnimal(List<T> animalList) {
+        Collections.sort(animalList, new AnimalComporator());
+        return animalList;
+    }
+}
